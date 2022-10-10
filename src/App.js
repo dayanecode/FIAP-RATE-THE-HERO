ac166185d795a00aa04989439c539e8c21e7ca02
Dayane Ramos <dayanecode@gmail.com>
2022-10-10 17:41:16 -0300

    cria componentes atômicos básicos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
 import { NotFound } from './screens/NotFound';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { NormalizeStyles } from './shared/NormalizeStyles'; 
+import { Header } from './common-components/Header/Header';
 
 export function App() {
 
-  return ( <BrowserRouter>
-    <Routes>
-      <Route path='/detalhes/:id' element={<Details/>} />
-      <Route path='/' element={<Search/>} />
-      <Route path='*' element={<NotFound/>} />
-    </Routes>
-  </BrowserRouter>
-  );
+  return (
+  <>
+    <NormalizeStyles />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/detalhes/:id' element={<Details/>} />
+        <Route path='/' element={<Search/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 }
 
 export default App;
+
+
 
