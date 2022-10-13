75067757772cb95223b3575407291b4f2e4867c2
dayanecode <dayanecode@gmail.com>
2022-10-13 02:43:39 -0300

    instala axios-hooks e cria skeleton loadings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Axios from 'axios';
+import { configure } from 'axios-hooks';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
 import { NotFound } from './screens/NotFound';
 import { NormalizeStyles } from './shared/NormalizeStyles'; 
 import { Header } from './common-components/Header/Header';
+
+const axios = Axios.create({
+  baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
+});
+
+configure({ axios });
 
 export function App() {
 
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
+import useAxios from 'axios-hooks';
+import React from 'react';
 import { Flex, Box } from 'reflexbox';
 import styled from 'styled-components';
 import { Button } from '../common-components/Button/Button';
 import { SearchField } from '../common-components/SearchField/SearchField';
 import { HeroCard } from '../components/HeroCard/HeroCard';
+import { HeroCardLoader } from '../components/HeroCard/HeroCardLoader';
 import { Spaces } from '../shared/DesignTokens';
-const HeroesGrid = styled(Box)`
+
+const HeroesGrid = styled(Box) `
 	display: grid;
 	grid-template-columns: 1fr;
 	gap: ${Spaces.ONE_HALF};
@@ -13,7 +18,45 @@ const HeroesGrid = styled(Box)`
 		gap: ${Spaces.TWO};
 	}
 `;
+
+async function searchHero(heroName) {
+	const { data } = await axios.get(`/search/${heroName}`, {
+		baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
+	});
+	return data.results || [];
+}
+
 export function Search() {
+	const [heroesOld, setHeroes] = React.useState([]);
+	const [search, setSearch] = React.useState({
+		value: 'captain',
+		doSearch: false
+	});
+	const [{data: heroes, loading: isLoadingHeroes }, searchHero] = useAxios (
+		`/search/${search.value}`,
+		{manual: true}
+	);
+
+	React.useEffect(() => {
+		searchHero();
+	}, []);
+
+	React.useEffect(() => {
+		if (search.doSearch) {
+			searchHero(search.value).then((heroes) => {
+				setHeroes(heroesOld);
+				setSearch((prevValue) => ({ ...prevValue, doSearch: false }));
+			});
+		}
+	}, [search]);
+	
+	function handleUpdateSearchValue({ target: { value } }) {
+		setSearch((prevValue) => ({ ...prevValue, value }));
+	}
+	function handleSearch() {
+		setSearch((prevValue) => ({ ...prevValue, doSearch: true }));
+	}
+	
 	return (
 		<>
 			<Flex
@@ -24,22 +67,49 @@ export function Search() {
 				mb={[Spaces.TWO, Spaces.FOUR]}
 			>
 				<Box flexGrow="1">
-					<SearchField placeholder="Digite um nome de herói ou heroína" />
+					<SearchField
+						placeholder="Digite um nome de herói ou heroína"
+						onKeyUp={handleUpdateSearchValue}
+					/>
 				</Box>
 				<Box ml={Spaces.TWO}>
-					<Button>Buscar</Button>
+					<Button onClick={handleSearch}>Buscar</Button>
 				</Box>
 			</Flex>
-			<HeroesGrid
-				px={[Spaces.ONE, Spaces.TWO]}
-				pb={[Spaces.ONE, Spaces.TWO]}
-			>
-				<HeroCard />
-				<HeroCard />
-				<HeroCard />
-				<HeroCard />
-				<HeroCard />
-			</HeroesGrid>
+			{!isLoadingHeroes && heroes && heroes.error ? (
+				<Box
+					px={[Spaces.ONE, Spaces.TWO]}
+					pb={[Spaces.ONE, Spaces.TWO]}
+				>
+					{/* <Alert type="info">
+						Nenhum registro de herói ou heroína foi encontrado.
+					</Alert> */}
+				</Box>
+			) : (
+				<HeroesGrid
+					px={[Spaces.ONE, Spaces.TWO]}
+					pb={[Spaces.ONE, Spaces.TWO]}
+				>
+					{isLoadingHeroes && (
+						<>
+							<HeroCardLoader />
+							<HeroCardLoader />
+							<HeroCardLoader />
+							<HeroCardLoader />
+						</>
+					)}
+					{!isLoadingHeroes && heroes && heroes.results.map((hero) => (
+							<HeroCard
+								key={hero.id}
+								id={hero.id}
+								secretIdentity={hero.biography['full-name']}
+								name={hero.name}
+								picture={hero.image.url}
+								universe={hero.biography.publisher}
+							/>
+						))}
+				</HeroesGrid>
+			)}
 		</>
-	);
+	);	
 }
