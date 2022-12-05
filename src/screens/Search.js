d4f7b885358d32d59023b58eb74a82c384e10dbd
dayanecode <dayanecode@gmail.com>
2022-12-04 22:41:44 -0300

    corrigida a pesquisa de detalhes

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,13 +1,12 @@
+import axios from 'axios';
 import React from 'react';
 import { Flex, Box } from 'reflexbox';
 import styled from 'styled-components';
 import { Button } from '../common-components/Button/Button';
-import { Alert } from '../common-components/Alert/Alert';
 import { SearchField } from '../common-components/SearchField/SearchField';
 import { HeroCard } from '../components/HeroCard/HeroCard';
 import { useHeroes } from '../hooks/useHeroes';
 import { Spaces } from '../shared/DesignTokens';
-import { HeroCardLoader } from '../components/HeroCard/HeroCardLoader';
 
 
 const HeroesGrid = styled(Box) `
@@ -22,32 +21,31 @@ const HeroesGrid = styled(Box) `
 `;
 
 async function searchHero(heroName) {
-	const { data } = await axios.get(`/search/${heroName}`, {
+	const {
+		data
+	} = await axios.get(`/search/${heroName}`, {
 		baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
 	});
 	return data.results || [];
 }
 
-export function Search() {
-	const [heroesOld, setHeroes] = React.useState([]);
-	const [search, setSearch] = React.useState({
-		value: 'captain',
-		doSearch: false
-	});
-	const [{data: heroes, loading: isLoadingHeroes }, searchHero] = useAxios (
-		`/search/${search.value}`,
-		{manual: true}
-	);
 
-	const { heroes, isLoadingHeroes, searchHero} = useHeroes(search.value)	
+	export function Search() {	
+		const [search, setSearch] = React.useState({
 
-	React.useEffect(() =>{
-		if (search.doSearch) {
-			searchHero().then(() => {
-				setSearch((prevValue) => ({ ...prevValue, doSearch: false}));
-			});
-		}
-	}, [search]);
+			value: 'captain',
+			doSearch: false
+		});	
+
+		const { heroes, isLoadingHeroes, searchHero } = useHeroes(search.value);
+	 
+		React.useEffect(() => {
+			if (search.doSearch) {
+				searchHero().then(() => {				
+					setSearch((prevValue) => ({ ...prevValue, doSearch: false }));
+				});
+			}
+		}, [search]);
 	
 	function handleUpdateSearchValue({ target: { value } }) {
 		setSearch((prevValue) => ({ ...prevValue, value }));
@@ -68,52 +66,26 @@ export function Search() {
 				<Box flexGrow="1">
 					<SearchField
 						placeholder="Digite um nome de herói ou heroína"
-						onKeyUp={handleUpdateSearchValue}
+						// onKeyUp={handleUpdateSearchValue}
 					/>
 				</Box>
 				<Box ml={Spaces.TWO}>
-					<Button onClick={handleSearch}>Buscar</Button>
+					<Button >Buscar</Button>
+					{/* <Button onClick={handleSearch}>Buscar</Button> */}
 				</Box>
 			</Flex>
-
-
-			{isLoadingHeroes && heroes && heroes.error ? (
-
-			<Box
-				px={[Spaces.ONE, Spaces.TWO]}
-				pb={[Spaces.ONE, Spaces.TWO]}
-			>
-				<Alert type="info">
-					Nenhum registro de herói ou heroína foi encontrado.
-				</Alert>
-			</Box>
-
-		): (
-			<HeroesGrid
-				px={[Spaces.ONE, Spaces.TWO]}
-				pb={[Spaces.ONE, Spaces.TWO]}
-				>
-
-					{isLoadingHeroes && (
-						<>
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-							<HeroCardLoader />
-						</>
-					)}
-
-					{!isLoadingHeroes && heroes && heroes.results.map((hero) => (
+			{heroes && (
+				<HeroesGrid>
+					{heroes.results.map((hero) => (
 							<HeroCard
 								key={hero.id}
 								id={hero.id}
 								secretIdentity={hero.biography['full-name']}
+								// secretIdentity={hero.secretIdentity}
 								name={hero.name}
 								picture={hero.image.url}
+								// picture={hero.picture}
+								// universe={hero.universe}
 								universe={hero.biography.publisher}
 							/>
 						))}
