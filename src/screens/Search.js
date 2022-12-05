e56880be61fa5220aea4455120e13ab3ef46095e
dayanecode <dayanecode@gmail.com>
2022-12-04 22:47:38 -0300

    inserted method onClick

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -66,10 +66,11 @@ async function searchHero(heroName) {
 				<Box flexGrow="1">
 					<SearchField
 						placeholder="Digite um nome de herói ou heroína"
+						onKeyUp={handleUpdateSearchValue}
 					/>
 				</Box>
 				<Box ml={Spaces.TWO}>
-					<Button >Buscar</Button>
+					<Button onClick={handleSearch}>Buscar</Button>
 				</Box>
 			</Flex>
 			{heroes && (
@@ -79,8 +80,11 @@ async function searchHero(heroName) {
 								key={hero.id}
 								id={hero.id}
 								secretIdentity={hero.biography['full-name']}
+								// secretIdentity={hero.secretIdentity}
 								name={hero.name}
 								picture={hero.image.url}
+								// picture={hero.picture}
+								// universe={hero.universe}
 								universe={hero.biography.publisher}
 							/>
 						))}
